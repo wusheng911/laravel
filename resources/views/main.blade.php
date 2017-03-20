60ff62a73861fd650c9eb1f267e905cb658fd07c
Silent Wang <silentwang@51chaohun.com>
2017-03-20 11:43:39 +0800

    mod flash file

---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<object type="application/x-shockwave-flash" 
+2   data="/flash/master.swf" 
+3   width="0" height="0">
+4   <param name="movie" value="your-flash-file.swf" />
+5   <param name="quality" value="high"/>
+6 </object>
+/* <embed src="/flash/hsf.swf"width="800" height="600"></embed> */
+</body>
+</html>
