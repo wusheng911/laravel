3389453a7db9781734af5e2760760fb73df1b46a
Silent Wang <silentwang@51chaohun.com>
2017-03-20 12:26:13 +0800

    merge master

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
@@ -13,21 +12,3 @@
 <embed allownetworking="none" allowscriptaccess="never" width="720" height="550" loop="false" menu="false" play="true" pluginspage="http://www.macromedia.com/go/getflashplayer" src="/flash/hsf.swf" type="application/x-shockwave-flash"wmode="window"></embed>
     </body>
 </html>
-
-
-=======
-<!doctype html>
-<html>
-<head>
-</head>
-<body>
-<object type="application/x-shockwave-flash" 
-2   data="/flash/master.swf" 
-3   width="0" height="0">
-4   <param name="movie" value="your-flash-file.swf" />
-5   <param name="quality" value="high"/>
-6 </object>
-/* <embed src="/flash/hsf.swf"width="800" height="600"></embed> */
-</body>
-</html>
->>>>>>> 60ff62a73861fd650c9eb1f267e905cb658fd07c
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,3 @@ Route::get('/','HomeController@index');
 /* Route::get('/', function () { */
 /*     return view('welcome'); */
 /* }); */
-<<<<<<< HEAD
-=======
-Route::get('/','HomeController@index');
->>>>>>> 60ff62a73861fd650c9eb1f267e905cb658fd07c
