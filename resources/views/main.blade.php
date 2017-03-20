124e91428152e77bc52cf08d43d1530fc373b3a6
Silent Wang <silentwang@51chaohun.com>
2017-03-20 12:23:25 +0800

    add flash file

---
+++ resources/views/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>测试flash</title>
+
+    </head>
+    <body>
+<embed allownetworking="none" allowscriptaccess="never" width="720" height="550" loop="false" menu="false" play="true" pluginspage="http://www.macromedia.com/go/getflashplayer" src="/flash/hsf.swf" type="application/x-shockwave-flash"wmode="window"></embed>
+    </body>
+</html>
+
+
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/','HomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Route::get('/', function () { */
+/*     return view('welcome'); */
+/* }); */
